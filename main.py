8ad8f8f3a8bb95234fb90c8f8aee5893fd8ae028
Richard Kirby <richard.james.kirby@gmail.com>
2021-08-09 21:17:36 +0100

    Initial Prototype version - lots of dead code, etc.

--- main.py
+++ main.py
@@ -20,8 +20,15 @@ if len(out.strip()) == 0:
 
 pi = pigpio.pi()
 
-pi.set_PWM_dutycycle(21, 255)
+pi.set_PWM_dutycycle(26, 255)
+pi.set_mode(21, pigpio.INPUT)
+pi.set_pull_up_down(21, pigpio.PUD_UP)
 
+
+def cbf(gpio, level, tick):
+   print(gpio, level, tick)
+
+tacho_cb = pi.callback(21, 0)
 
 class FanControl(threading.Thread):
 
@@ -71,8 +78,36 @@ class FanControl(threading.Thread):
 # Set up a few fan controllers such that there are some to choose from.
 
 try:
+    target_rpm = 1800
+    gain = 0.01
 
-    fan_control1 = FanControl("Every 2 minutes(test)", pi, 21, duty=100, duration=120, time_delay=120)
+    print(target_rpm, gain)
+    current_pwm = 0
+
+    while True:
+        actual_rpm = tacho_cb.tally() * 30
+        error = target_rpm - actual_rpm
+        current_pwm = min(max(current_pwm + gain * error,0), 255)
+
+        print("Target {} PWM Cmd {:.2f} PWM % {:.2f} RPM {}".format(target_rpm, current_pwm, current_pwm/255 * 100,actual_rpm))
+        tacho_cb.reset_tally()
+        pi.set_PWM_dutycycle(26, current_pwm)
+        time.sleep(1)
+
+
+    for i in range(0, 255):
+        print("PWM Cmd {} PWM % {:.2f} RPM {}".format(i, i/255 * 100, tacho_cb.tally()*30))
+        tacho_cb.reset_tally()
+        pi.set_PWM_dutycycle(26, i)
+        time.sleep(1)
+
+    pi.set_PWM_dutycycle(26,0)
+
+
+
+    time.sleep(5)
+
+    fan_control1 = FanControl("Every 2 minutes(test)", pi, 26, duty=100, duration=120, time_delay=120)
     fan_control1.daemon = True
     fan_control1.start()
 
@@ -84,7 +119,7 @@ try:
     fan_control3.daemon = True
     fan_control3.start()
 
-    fan_control4 = FanControl("Every 4 hours", pi, 26, duty=255, duration=120, time_delay=60*60*4)
+    fan_control4 = FanControl("Every 4 hours", pi, 27, duty=255, duration=120, time_delay=60*60*4)
     fan_control4.daemon = True
     fan_control4.start()
 
@@ -109,7 +144,7 @@ except:
     raise
 
 finally:
-    pi.set_PWM_dutycycle(21, 0)
+    pi.set_PWM_dutycycle(26, 0)
     pi.stop()
 
 
